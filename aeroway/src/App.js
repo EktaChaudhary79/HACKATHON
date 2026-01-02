223fa1264855d0f177337a4a789b24d8548a6539
EktaChaudhary79 <chaudharyekta79@gmail.com>
2026-01-02 23:05:59 +0530

    Added aeroway React app to Hackathon repo

---
+++ aeroway/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MapComponent from './MapComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to AeroWay</h1>
+      <MapComponent />
+    </div>
+  );
+}
+
+export default App;
+
